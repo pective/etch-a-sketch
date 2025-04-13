29284c2a89b6355632d67b478057d798c96671b5
pective <pective@proton.me>
2025-04-13 14:15:12 +0200

    Added functionality to the random color button

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let size = 16;
 
 const gridSize = document.querySelector(".gridSizeButton");
+const randomColor = document.querySelector(".randomColorButton");
+const ereaser = document.querySelector(".ereaserButton");
 let screen = document.querySelector(".screen");
 
 gridSize.addEventListener('click', () => {
@@ -8,6 +10,28 @@ gridSize.addEventListener('click', () => {
     screen.innerHTML = '';
     makeGrid(size);
 });
+
+let randomColorMode = false;
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+ereaser.addEventListener('click')
+
+randomColor.addEventListener('click', () => {
+    if (randomColorMode === false) {
+        randomColorMode = true;
+        randomColor.style.backgroundColor = "#0a459e"
+    } else {
+        randomColorMode = false;
+        randomColor.style.backgroundColor = "#444444"
+    }
+})
 
 function makeGrid(size) {
     for (let i = 0; i < size; i++) {
@@ -18,7 +42,9 @@ function makeGrid(size) {
             row.classList.add("row");
             row.style.border = "1px solid black";
             row.addEventListener('mouseover', () => {
-                row.style.backgroundColor = "#444444";
+                if( randomColorMode === true ) {
+                    row.style.backgroundColor = getRandomColor();
+                } else row.style.backgroundColor = "#444444";
             })
             column.appendChild(row);
         }
